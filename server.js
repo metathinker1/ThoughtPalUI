aa6f9165557bd473fe9ceafcbd384eec00675b31
Robert Wood <rob.a.wood@gmail.com>
2017-09-15 22:11:14 -0600

    Get directories from SimpleNoteDocServer

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 const connect = require('connect'),
   serveStatic = require('serve-static'),
   open = require('open'),
-  request = require('request-promise');
+  request = require('request-promise'),
+  jsonProxy = require('./proxy');
 
 const openBrowserOnListen = true;
 
 const app = connect()
 
+app.use(jsonProxy("/jsonproxy"));
 app.use(serveStatic(__dirname), {dotfiles:'ignore'})
 
 app.use('/test', function getDirectoriesHandler(req, res, next) {
@@ -15,16 +17,6 @@ app.use('/test', function getDirectoriesHandler(req, res, next) {
   res.end('')
 });
 
-function getDirectories() {
-  const options = {
-    method: 'GET',
-    uri: 'http://localhost:5011/get-directories'
-  }
-  request(options)
-    .then(function getDirectoriesHandler(data) {
-      console.log(data);
-    });
-}
 
 app.listen(5012, function function_name(argument) {
   if (openBrowserOnListen) open('http://localhost:5012/')
